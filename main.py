0580005a7ec5d98dbc60e49531d1cf6ac2ccbeb1
Darold McKnight <mcknightdarold33@yahoo.com>
2023-03-14 10:38:58 -0500

    Add database name to "create_server_connection" function and calling statement.

---
+++ main.py
@@ -0,0 +1,45 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_server_connection(host_name, user_name, user_password,db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            passwd = user_password,
+            database = db_name
+        )
+        print("MySQL Database Connection Successful")
+    except Error as err:
+        print(f"Error {err}")
+    return connection
+
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: {err}")
+
+#Queries
+create_database_query = "create database EXOTIC_DEALERSHIP"
+#calling statement
+connection = create_server_connection("localhost", "root", "student")
+#call create_database function to create DB in mySQL
+create_database(connection, create_database_query)
+
+connection = create_server_connection("localhost", "root", "student","exotic_dealership")
+
+
+
+
+
+
+
+
+
+#calling statement
+connection = create_server_connection("localhost", "root", "student")
